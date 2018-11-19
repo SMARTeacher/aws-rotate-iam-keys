ac272e960a9670df3f6914a9375ab7746943d7e5
Alexandre Garcia <alexandre.garcia@prodigygame.com>
2018-11-19 15:27:44 -0500

    v0.8.3

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -1,6 +1,6 @@
 class AwsRotateIamKeys < Formula
   desc "Automatically set up a cron job to rotate your IAM keys"
-  url "https://github.com/SMARTeacher/aws-rotate-iam-keys/archive/v0.8.2.tar.gz"
+  url "https://github.com/SMARTeacher/aws-rotate-iam-keys/archive/v0.8.3.tar.gz"
   sha256 "07bd5068232b7f4434222a954223f29039f43a5d097769c2d2800220c552b1ea"
   depends_on "awscli"
   depends_on "jq"
