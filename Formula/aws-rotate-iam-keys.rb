35ca6d0e2027a6ae3ea786fa78de5578497e2eed
Neetika Bhargava <43829189+neetika-bhargava@users.noreply.github.com>
2018-11-19 11:39:17 -0500

    Update the URL

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -1,7 +1,7 @@
 class AwsRotateIamKeys < Formula
   desc "Automatically set up a cron job to rotate your IAM keys"
   homepage "https://aws-rotate-iam-keys.com"
-  url "https://github.com/rhyeal/aws-rotate-iam-keys/archive/v0.8.1.tar.gz"
+  url "https://github.com/SMARTeacher/aws-rotate-iam-keys/archive/v0.8.1.tar.gz"
   sha256 "bff7a999f402db12114fae91d46455e5f36b9559fd4a07caad09c5f42a99b8d6"
   depends_on "awscli"
   depends_on "jq"
