49d7d522a59f86750b7d120d1ea456f4e7a672f5
Alexandre Garcia <alexandre.garcia@prodigygame.com>
2018-11-19 15:23:58 -0500

    update sha for homebrew 🤷

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -1,7 +1,7 @@
 class AwsRotateIamKeys < Formula
   desc "Automatically set up a cron job to rotate your IAM keys"
   url "https://github.com/SMARTeacher/aws-rotate-iam-keys/archive/v0.8.2.tar.gz"
-  sha256 "bff7a999f402db12114fae91d46455e5f36b9559fd4a07caad09c5f42a99b8d6"
+  sha256 "07bd5068232b7f4434222a954223f29039f43a5d097769c2d2800220c552b1ea"
   depends_on "awscli"
   depends_on "jq"
   depends_on "gnu-getopt"
